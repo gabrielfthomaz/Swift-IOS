be7975587bb76c4064145501a4715b04d6281d0c
gabrielfthomaz <103137127+gabrielfthomaz@users.noreply.github.com>
2022-08-06 09:59:26 -0300

    CalculadoraIMC - Atualizada
    Alert incluído

--- hackatruckCalcIMC/hackatruckCalcIMC/ViewController.swift
+++ hackatruckCalcIMC/hackatruckCalcIMC/ViewController.swift
@@ -13,29 +13,42 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var textoPeso: UITextField!
     
+     @IBOutlet weak var textoResultado: UILabel!
     
     @IBOutlet weak var textoAltura: UITextField!
     
-    
     @IBAction func botaoEnviar(_ sender: Any) {
         
-        let pesoOK = Double(textoPeso.text!)
-        let alturaOk = Double(textoAltura.text!)
+//        let pesoOK = Double(textoPeso.text!)
+//        let alturaOk = Double(textoAltura.text!)
+//
+//        let resultado = pesoOK! / (alturaOk! * alturaOk!)
+//        textoResultado.text = String(resultado)
         
-        let resultado = pesoOK! / (alturaOk! * alturaOk!)
-        textoResultado.text = String(resultado)
+        
+        if (textoPeso.text!.isEmpty)||(textoAltura.text!.isEmpty) == true {
+
+
+           let alert = UIAlertController(title: "Atenção", message: "Informe o Peso e a Altura.", preferredStyle: UIAlertController.Style.alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+
+           present(alert, animated: true, completion: nil)
+            
+       }else{
+            
+            let pesoOK = Double(textoPeso.text!)
+            let alturaOk = Double(textoAltura.text!)
+            
+            let resultado = pesoOK! / (alturaOk! * alturaOk!)
+            textoResultado.text = String(resultado)
     }
-    
-    
-    @IBOutlet weak var textoResultado: UILabel!
-    
-    
+}
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
+
 
