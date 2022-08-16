7d1f75d8c7607d636227b32be93f68be2400e08d
gabrielfthomaz <103137127+gabrielfthomaz@users.noreply.github.com>
2022-08-16 19:34:03 -0300

    swift-IoT

---
+++ AppIOT/AppIOT/EstacionamentoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EstacionamentoTableViewCell.swift
+//  AppIOT
+//
+//  Created by Student on 16/08/22.
+//  Copyright © 2022 Felipe Corte. All rights reserved.
+//
+
+import UIKit
+
+class EstacionamentoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
